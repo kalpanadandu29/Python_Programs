bcecfd8c4bbfb7d374156ed6ba1b146a69c6cd77
kalpanadandu29 <164886949+kalpanadandu29@users.noreply.github.com>
2024-07-19 19:37:00 +0530

    Add files via upload

---
+++ Basic_Programs/GreatestOfTwoNumbers.py
@@ -0,0 +1,17 @@
+n1=int(input("enter the number1"))
+n2=int(input("enter the number2"))
+if(n1>n2):
+    print(n1)
+else:
+    print(n2)
+
+
+
+
+#another program for above problem
+
+n1=int(input("enter the number1"))
+n2=int(input("enter the number2"))
+print(max(n1,n2))
+
+
